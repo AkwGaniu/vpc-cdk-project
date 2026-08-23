8aa1e37ed3a9c5b85655fbea189cb19906bab474
Ganiu Akownanu <45771378+AkwGaniu@users.noreply.github.com>
2026-08-23 17:24:39 +0100

    created vpc with 2 AZs and 1 public and private subnet in each AZ

--- lib/vpc-cdk-project-stack.ts
+++ lib/vpc-cdk-project-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class VpcCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a VPC with 2 public and 2 private subnets
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2, // Default is all AZs in the region
+      subnetConfiguration: [
+        {
+          name: 'PublicSubnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24
+        },
+        {
+          name: 'PrivateSubnet',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24
+        }
+      ]
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'VpcCdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Output the VPC ID
+    new cdk.CfnOutput(this, 'VpcId', {
+      value: vpc.vpcId,
+      description: 'The created VPC ID',
+      exportName: 'VpcId'
+    });
   }
 }
